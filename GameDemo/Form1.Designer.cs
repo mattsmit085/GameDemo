4c99db6d131765ad1919906446d099e9e201b2d3
Matt Smith <mattsmit085@outlook.com>
2021-02-09 09:51:39 -0500

    Game

---
+++ GameDemo/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace GameDemo
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.Countdownlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("MS Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(151, 24);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(167, 35);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "GAME LOL";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(120, 75);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(227, 90);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(120, 171);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(227, 90);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Countdownlabel
+            // 
+            this.Countdownlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Countdownlabel.Location = new System.Drawing.Point(133, 112);
+            this.Countdownlabel.Name = "Countdownlabel";
+            this.Countdownlabel.Size = new System.Drawing.Size(200, 120);
+            this.Countdownlabel.TabIndex = 4;
+            this.Countdownlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Countdownlabel.Visible = false;
+            this.Countdownlabel.Click += new System.EventHandler(this.Countdownlabel_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(492, 303);
+            this.Controls.Add(this.Countdownlabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Game";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label Countdownlabel;
+    }
+}
+
---
+++ GameDemo/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace GameDemo
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.countdown);
+
+            alertPlayer.Play();
+            Refresh();
+            Thread.Sleep(750);
+            playButton.Visible = false;
+            exitButton.Visible = false;
+            Countdownlabel.Visible = true;
+            Countdownlabel.Text = "5";
+            Refresh();
+            Thread.Sleep(1000);
+            Countdownlabel.Text = "4";
+            Refresh();
+            Thread.Sleep(1000);
+            Countdownlabel.Text = "3";
+            Refresh();
+            Thread.Sleep(1000);
+            Countdownlabel.Text = "2";
+            Refresh();
+            Thread.Sleep(1000);
+            Countdownlabel.Text = "1";
+            Refresh();
+            Thread.Sleep(1000);
+            Countdownlabel.TextAlign = ContentAlignment.MiddleCenter;
+            Countdownlabel.Text = "GO!";
+            BackColor = Color.Green;
+            titleLabel.Visible = false;
+            Refresh();
+            Thread.Sleep(500);
+            SoundPlayer songPlayer = new SoundPlayer(Properties.Resources.All_Time_High_Jump___8_BitChiptune___Royalty_Free_Music);
+
+            songPlayer.Play();
+            Refresh();
+            Thread.Sleep(1000);
+            playerLabel.Visible = true;
+            Refresh();
+           
+        }
+
+        private void Countdownlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
